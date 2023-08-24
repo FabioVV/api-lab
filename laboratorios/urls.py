0c973bce0e12aa9552b7c747ccb430aad5910ee5
Fábio Varela <fabio-varela@protonmail.com>
2023-08-24 20:23:53 -0300

    adicionando routers

--- laboratorios/urls.py
+++ laboratorios/urls.py
@@ -9,16 +9,30 @@
 
 # urlpatterns = router.urls
 
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import SimpleRouter
 from . import views
+
+
+# FORMA MAIS FACIL DE GERAR TODAS AS ROTAS
+router = SimpleRouter()
+router.register('', views.LaboratorioV2viewset, basename='laboratorios-api')
+
+
 
 urlpatterns = [
     #FUNCTION BASED
     path('laboratorios/api/v1/', views.laboratorio, name = 'Laboratorios'),
     path('laboratorios/api/v1/<int:id>/', views.laboratorio_detail, name = 'Laboratorio'),
+    
 
     #CLASS BASED
-    path('laboratorios/api/v2/', views.LaboratorioV2viewset.as_view({'get':'list', 'post':'create'}), name = 'Laboratorios'),
-    path('laboratorios/api/v2/<int:id>/', views.LaboratorioV2viewset.as_view({'get':'retrieve', 'patch':'partial_update', 'delete':'destroy', 'put':'update'}), name = 'Laboratorio'),
+    # path('laboratorios/api/v2/', views.LaboratorioV2viewset.as_view({'get':'list', 'post':'create'}), name = 'Laboratorios'),
+    # path('laboratorios/api/v2/<int:id>/', views.LaboratorioV2viewset.as_view({'get':'retrieve', 'patch':'partial_update', 'delete':'destroy', 'put':'update'}), name = 'Laboratorio'),
+
+
+    #USING ROUTERS +++++  FORMA MAIS FACIL DE GERAR TODAS AS ROTAS
+    path('laboratorio/api/v3/', include(router.urls))
 ]
 
+#urlpatterns += router.urls
