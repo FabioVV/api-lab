b59c71b6ffeefcb885f3910e4653004151b91d6f
FabioVV <fabio-varela@protonmail.com>
2023-10-05 15:56:33 -0300

    fixed permissions #2

--- reservas/permissions.py
+++ reservas/permissions.py
@@ -1,8 +1,9 @@
 from rest_framework import permissions
+from usuarios.models import Usuario_tipo
 
 class IsPowerUser(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
-        return request.user.user_type == 2 or request.user.id == obj.user.id
+        return request.user.user_type == Usuario_tipo.objects.get(id = 2)  or request.user.id == obj.user.id
     
     
     def has_permission(self, request, view):
